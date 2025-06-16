9d2646228b2580d3d5a4b9065fd1c8da0a1fa161
Ahmed Mohamed Khalid <103767283+AhmedSocrates@users.noreply.github.com>
2025-06-16 14:22:20 +0800

    Update Level.java
    Updated Level logic

--- Level.java
+++ Level.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.util.ArrayList;
-
 import javax.swing.ImageIcon;
 
 public class Level {
@@ -12,63 +11,82 @@ public class Level {
     private ArrayList<Image> backgroundImages;
 
     public Level(int levelID) {
-        if(levelID == 1){
-            
-        }
-        else if (levelID == 2){
+        this.levelID = levelID;
+        platforms = new ArrayList<>();
+        coins = new ArrayList<>();
+        enemies = new ArrayList<>();
+        obstacles = new ArrayList<>();
+        backgroundImages = new ArrayList<>();
 
+        switch (levelID) {
+            case 1: {
+                loadLevel1();
+            }
+            case 2: {
+                loadLevel2();
+            }
+            case 3: {
+                loadLevel3();
+            }
+            case 4: {
+                loadLevel4();
+            }
         }
-        else if(levelID == 3){
-
-        }
-        else if(levelID == 4){
-
-        }
-
     }
 
-    public void levels(int levelID){
+    private void loadLevel1() {
+        platforms.add(new Platform(0, 500, 4000, 350, false));
+        platforms.add(new Platform(300, 400, 200, 20, true));
+        platforms.add(new Platform(600, 340, 200, 20, true));
+        platforms.add(new Platform(950, 420, 150, 20, true));
 
-        if (levelID == 1){
-        platforms = new ArrayList<>();
-        platforms.add(new Platform(0, 500, 4000, 350,false));
-        platforms.add(new Platform(300, 400, 200, 20,true));
-        platforms.add(new Platform(600, 340, 200, 20,true));
-        platforms.add(new Platform(950, 420, 150, 20,true));
-
-        coins = new ArrayList<>();
         coins.add(new Coin(350, 380));
         coins.add(new Coin(650, 320));
 
-        enemies = new ArrayList<>();
         enemies.add(new Enemy(1000, 360));
         enemies.add(new Enemy(1200, 440));
         enemies.add(new Enemy(1800, 440));
 
-        obstacles = new ArrayList<>();
         obstacles.add(new Obstacle(1600, 450));
 
-        backgroundImages = new ArrayList<>();
         backgroundImages.add(new ImageIcon(getClass().getResource("/Library.png")).getImage());
         backgroundImages.add(new ImageIcon(getClass().getResource("/World1.png")).getImage());
         backgroundImages.add(new ImageIcon(getClass().getResource("/World2.png")).getImage());
         backgroundImages.add(new ImageIcon(getClass().getResource("/Library.png")).getImage());
         backgroundImages.add(new ImageIcon(getClass().getResource("/World2.png")).getImage());
         backgroundImages.add(new ImageIcon(getClass().getResource("/World1.png")).getImage());
-        } 
-        else if (levelID == 2){
-
-        }
-        else if(levelID == 3){
-
-        }
-        else if(levelID == 4){
-
-        }
+    }
+    public void loadLevel2(){
 
     }
-    public void draw(Graphics g) {
-        g.setColor(new Color(200, 255, 200));
+    public void loadLevel3(){
+
+    }
+    public void loadLevel4(){
+
+    }
+
+    public ArrayList<Platform> getPlatforms() {
+        return platforms;
+    }
+
+    public ArrayList<Coin> getCoins() {
+        return coins;
+    }
+
+    public ArrayList<Enemy> getEnemies() {
+        return enemies;
+    }
+
+    public ArrayList<Obstacle> getObstacles() {
+        return obstacles;
+    }
+
+    public ArrayList<Image> getBackgroundImages() {
+        return backgroundImages;
+    }
+}
+
         g.fillRect(0, 0, 2000, 600);
     }
 }
